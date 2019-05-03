979a42163f3055443f6c0a8831ad70a678f2b533
FJJLeon <695048014@qq.com>
2019-05-03 18:59:46 +0800

    add init loop test

--- src/Calculator/CalculatorTest.java
+++ src/Calculator/CalculatorTest.java
@@ -43,12 +43,22 @@ public class CalculatorTest {
 	public void testInitCover() {
 		assertEquals(cal.drive("1+2").run(), "3.0");
 		assertEquals(cal.drive("cos(0)").run(), "1.0");
-		assertEquals(cal.drive("(2[+0)").run(), "error");
+		assertEquals(cal.drive("1+(2[+0)").run(), "error");
 	}
 	
 	@Test
 	public void testInitLoop() {
-		
+		// keep outer loop 2~4, test inner loop times
+		// due to inner loop for method para, times <= 2
+		assertEquals(cal.drive("()").run(), "error");   	// inner 0, fix bug
+		assertEquals(cal.drive("cos(0)").run(), "1.0");		// inner 1
+		assertEquals(cal.drive("(2*2)").run(), "4.0");  	// inner 2
+		// keep inner loop 0~1, test outer loop times
+		assertEquals(cal.drive("").run(), "error"); 		// outer 0
+		assertEquals(cal.drive("1").run(), "1");			// outer 1
+		assertEquals(cal.drive("cos(1)*").run(), "error"); 	// outer 2
+		assertEquals(cal.drive("2+cos(0)").run(), "3.0");	// outer 3
+		assertEquals(cal.drive("0+cos(0)*2-3/6").run(), "1.5");	// outer N
 	}
 	
 	@Test
